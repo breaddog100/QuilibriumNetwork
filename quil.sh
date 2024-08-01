99caffa696754b57c487f9a58e813f36bef27011
kimi <kimi@kimideMacBook-Air.local>
2024-08-01 15:22:25 +0800

    update

--- quil.sh
+++ quil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置版本号
-current_version=20240801001
+current_version=20240801002
 
 update_script() {
     # 指定URL
@@ -158,7 +158,7 @@ function backup_key(){
 	fi
 	
 	# 创建压缩文件
-	zip -r ~/quil_bak_$(date +%Y%m%d).zip .config
+	zip -r ~/quil_bak_$(hostname)_$(date +%Y%m%d).zip .config
 	echo "已将 .config目录压缩并保存到$HOME下"
 
 }
