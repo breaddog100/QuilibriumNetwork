2a1eee907af21b598900d8e0576763c4c70e4c48
kimi <kimi@kimideMacBook-Air.local>
2024-08-14 17:43:00 +0800

    update

--- backup.sh
+++ backup.sh
@@ -6,8 +6,8 @@ sudo mkdir /shared
 sudo chown -R ubuntu:ubuntu /shared
 sudo mount -t nfs 10.10.10.22:/shared /shared
 mkdir /shared/$(hostname)
-/bin/zip -r /shared/$(hostname)/quil_bak_$(hostname)_$(date +%Y%m%d%H%M%S).zip $HOME/ceremonyclient/node/.config
-(crontab -l 2>/dev/null; echo "0 */12 * * * /bin/zip -r /shared/\$(hostname)/quil_bak_\$(hostname)_\$(date +\%Y\%m\%d\%H\%M\%S).zip \$HOME/ceremonyclient/node/.config") | crontab -
+zip -r /shared/$(hostname)/quil_bak_$(hostname)_$(date +%Y%m%d%H%M%S).zip $HOME/ceremonyclient/node/.config
+(crontab -l 2>/dev/null; echo "0 */12 * * * zip -r /shared/\$(hostname)/quil_bak_\$(hostname)_\$(date +\%Y\%m\%d\%H\%M\%S).zip \$HOME/ceremonyclient/node/.config") | crontab -
 ls /shared
 ls -lrth /shared/$(hostname)
 crontab -l
