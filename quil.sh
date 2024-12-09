8d9b2273d01004896fcfd7486ba55808669777a4
breaddog100 <meteorlmd@gmail.com>
2024-12-09 13:59:58 +0800

    update

--- quil.sh
+++ quil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置版本号
-current_version=20241209003
+current_version=20241209004
 
 # Colors for output
 RED='\033[0;31m'
@@ -223,7 +223,7 @@ function check_balance(){
 	cd ~/ceremonyclient/client
 	qclient_file=$(last_bin_file "qclient")
 	echo $qclient_file
-	"$qclient_file" --config $CONFIG_PATH token balance --public-rpc
+	"$qclient_file" --config $CONFIG_PATH token coins --public-rpc
 
 }
 
